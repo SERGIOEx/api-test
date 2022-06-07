8a9952ce5028bc0bf46b0bc256dde281fff532c4
Sergio K <sergioexua@gmail.com>
2022-06-07 23:45:32 +0300

    some edits

---
+++ src/app/Containers/Auth/Http/Requests/RecoverPasswordRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Containers\Auth\Http\Requests;
+
+use App\Core\Parents\Requests\ApiRequest;
+
+class RecoverPasswordRequest extends ApiRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'email'      => 'required|string|email|',
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+}
